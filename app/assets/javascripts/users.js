e299afc1c356af2a56b1ceb0058546c06f629f0a
markjconlon <markjconlon@gmail.com>
2017-08-24 23:14:06 -0400

    added a listener to the email field on keyup and after 750ms of inactivity it calls the validateEmail function that checks agains a regexp to ensure proper formatthen it makes the ajax request if the email is of proper format, the ajax request is first sent to check_email function in users_controller which makes the request via the clearbit gem, the desired values are extracted and the response finally returns to the client

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,51 @@
 $( document ).on('turbolinks:load', function() {
+  function validateEmail (email) {
+    let errors = {}
+    let re = new RegExp('[^@]+@[^@]+\\.[^@]+')
+    if (!re.test(email)) {
+      errors['nope'] = 'Email is incorrectly formatted'
+    }
 
+    return Object.keys(errors).length === 0
+  }
+
+  function revealFields(){
+    $('#fullNameDiv').removeClass("hidden")
+    $('#companyNameDiv').removeClass("hidden")
+    $('#employeesDiv').removeClass("hidden")
+    $('#phoneDiv').removeClass("hidden")
+  }
+
+  var debounceTimer;
+  $('#email').on('keyup', function () {
+    email = this.value
+    clearTimeout( debounceTimer );
+    debounceTimer = setTimeout( function() {
+      if (validateEmail(email)) {
+        $.ajax({
+          url: '/users/check_email',
+          method: 'GET',
+          dataType: 'json'
+        }).done(function(data){
+          // when the request comes back it remove hidden class from form fields
+          revealFields();
+          console.log(data);
+          // fill the fields with the data that comes back, UsersController
+          // already protects against nothing for the corressponding key and nilClass
+          $('#fullName').val(data.person.fullName);
+          $('companyName').val(data.person.companyName);
+          $('#employees').val(data.person.employees);
+          $('#phone').val(data.person.phone);
+        }).fail(function(){
+          // if nothing is returned whether it be from an internal error or
+          // an error at Clearbit reveal the fields so the client can still
+          // complete the form
+          revealFields();
+        });
+      } else {
+        console.log('nope not valid')
+      }
+
+    }, 750);
+  })
 });
