ccbdd0424c3d46183c9b1ce6862ace38861c694b
markjconlon <markjconlon@gmail.com>
2017-08-24 22:51:22 -0400

    changed users.coffee to users.js, added the turbolinks ready function, added display none to css to hide form fields until they are needed, added new controller action to userscontroller, added check_email function which sends a request to clearbit through the gem, added routes for users_controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def check_email
+    @result = Clearbit::Enrichment.find(email: 'markjconlon@gmail.com', stream: true)
+    if request.xhr?
+      render :json =>
+      {person: {fullName: "#{@result.person.name.fullName}",
+                companyName: "#{@result.person.employment.name}",
+                phone: "#{@result.company ? @result.company.phone : ""}",
+                employees: "#{@result.company ? @result.company.employees : ""}"
+      }}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new]
+  get '/users/check_email/' => 'users#check_email'
 end
